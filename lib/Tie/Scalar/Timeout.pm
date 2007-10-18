738922f93477c9efa8c356841cd84484f06c90be
Marcel Grünauer <marcel@cpan.org>
2007-10-18 17:51:47 +0000

    Import of MARCEL/Tie-Scalar-Timeout-1.33 from CPAN.
    gitpan-cpan-distribution: Tie-Scalar-Timeout
    gitpan-cpan-version:      1.33
    gitpan-cpan-path:         MARCEL/Tie-Scalar-Timeout-1.33.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/Tie/Scalar/Timeout.pm
+++ lib/Tie/Scalar/Timeout.pm
@@ -5,7 +5,7 @@ use strict;
 use base 'Tie::Scalar';
 use Time::Local;
 
-our $VERSION = '1.3.2';
+our $VERSION = '1.33';
 
 sub TIESCALAR {
 	my $class = shift;
@@ -200,6 +200,19 @@ variable to a different value, for example.
 
 =back
 
+=head1 TAGS
+
+If you talk about this module in blogs, on del.icio.us or anywhere else,
+please use the C<tiescalartimeout> tag.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+Please report any bugs or feature requests to
+C<bug-tie-scalar-timeout@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.
+
 =head1 INSTALLATION
 
 See perlmodinstall for information and options on installing Perl modules.
@@ -207,31 +220,22 @@ See perlmodinstall for information and options on installing Perl modules.
 =head1 AVAILABILITY
 
 The latest version of this module is available from the Comprehensive Perl
-Archive Network (CPAN).  Visit <http://www.perl.com/CPAN/> to find a CPAN
-site near you.  Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
-
-=head1 VERSION
-
-This document describes version 1.3.2 of Tie::Scalar::Timeout.
-
-=head1 BUGS
-
-None known so far. If you find any bugs or oddities, please do tell me
-about them.
+Archive Network (CPAN). Visit <http://www.perl.com/CPAN/> to find a CPAN
+site near you. Or see <http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
 
 =head1 AUTHOR
 
-Marcel GrE<uuml>nauer <marcel@cpan.org>
+Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
 
-=head1 COPYRIGHT
+The code is almost completely lifted from L<Plagger>, so really Tatsuhiko
+Miyagawa C<< <miyagawa@bulknews.net> >> deserves all the credit.
 
-Copyright 2000-2003 Marcel GrE<uuml>nauer. All rights reserved.
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2000-2007 by Marcel GrE<uuml>nauer
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
-=head1 SEE ALSO
-
-perl(1), Tie::Scalar(3pm).
-
 =cut
+
